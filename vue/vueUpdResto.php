79c6a4019a69afed780f5942116b09b7ccd42f40
Xorgynoth <xorgynoth@gmail.com>
2021-12-14 11:19:22 +0100

    Itération 4

--- vue/vueUpdResto.php
+++ vue/vueUpdResto.php
@@ -14,36 +14,57 @@ use modele\dao\RestoDAO;
 if(isset($_GET["idR"])) {
     $resto = RestoDAO::getOneById($_GET["idR"]);
     ?>
-    <h1>Modifier restaurant</h1>
+    <h1>Ajouter un restaurant</h1>
+    <br>
+    <h3><font color="orange">* Obligatoire</font></h3>
+    <br>
     <form action="./?action=ajouterResto" method="POST">
 
+        <p>Nom : <font color="orange">*</font></p>
         <input type="text" name="nomR" placeholder="Nom" /><br />
+
+        <p>Numéro de rue : <font color="orange">*</font></p>
         <input type="text" name="numAdr" placeholder="Numéro rue"  /><br />
+
+        <p>Vooie de la rue : <font color="orange">*</font></p>
         <input type="text" name="voieAdr" placeholder="Voie rue" /><br />
+
+        <p>Code postal : <font color="orange">*</font></p>
         <input type="text" name="cpR" placeholder="Code postal" /><br />
+
+        <p>Ville : <font color="orange">*</font></p>
         <input type="text" name="villeR" placeholder="Ville" /><br />
+
+        <p>Latitude :</p>
         <input type="text" name="latitudeDegR" placeholder="Latitude" /><br />
+
+        <p>Longitude :</p>
         <input type="text" name="longitudeDegR" placeholder="Longitude" /><br />
+
+        <p>Description : <font color="orange">*</font></p>
         <input type="text" name="descR" placeholder="Description" /><br />
+
+        <p>Horaire :</p>
         <input type="text" name="horairesR" placeholder="Horaire" /><br />
-        <input type="text" name="photoR" placeholder="Chemin photo" /><br />
+        <!--<input type="text" name="photoR" placeholder="Chemin photo" /><br />-->
 
-        <br/>
-        <br/>
+        <br />
+        <br />
 
-        Les types de cuisine déjà selectionnés : <br />
+        Choisir d'autres types de cuisine : <br />
         <ul id="tagFood">
             <?php
-            for ($i = 0; $i < count($mesTypeCuisinePreferes); $i++) {
-                $unTC = $mesTypeCuisinePreferes[$i];
+            for ($i = 0; $i < count($lesAutresTypesCuisine); $i++) {
+                $unTC = $lesAutresTypesCuisine[$i];
                 ?>
-                <input type="checkbox" name="delLstidTC[]" id="delType<?= $i ?>" value="<?= $unTC->getIdTC() ?>" >
-                <label for="delType<?= $i ?>"><li class="tag"><span class="tag">#</span><?= $unTC->getLibelleTC() ?></li></label><br />
+                <input type="checkbox" name="addLstidTC[]" id="addType<?= $i ?>" value="<?= $unTC->getIdTC() ?>" >
+                <label for="addType<?= $i ?>"><li class="tag"><span class="tag">#</span><?= $unTC->getLibelleTC() ?></li></label><br />
             <?php } ?>
         </ul>
         <br />
-        <p>Pas terminé</p>
-        <br /><br />
+        <br />
+
+        <input type="submit" value="Ajouter" />
     </form>
 <?php
 } else {
